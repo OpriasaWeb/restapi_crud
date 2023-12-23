65d5d5eda56a16d751b37a14edaa91cd51ec4624
Jeremy Opriasa <opriasajbusiness@gmail.com>
2023-12-23 17:09:44 +0800

    fix: add put and delete router from controlller

--- routes/posts.router.js
+++ routes/posts.router.js
@@ -6,6 +6,8 @@ const postsController = require("../controller/posts.controller")
 router.get("/", postsController.getAll)
 router.get("/:id", postsController.getById)
 router.post("/", postsController.create)
+router.put("/:id", postsController.update)
+router.delete("/:id", postsController.delete)
 
 module.exports = router;
 
